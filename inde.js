4c61443611f0de5397c203d7a77e64515eccfd98
unknown <abdsaad1162002@gmail.com>
2018-06-26 00:00:22 +0300

    .

---
+++ inde.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express   = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+const app = express();
+
+app.set('port' , (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/' , (req ,res)=>{
+	res.send("Hi Im a ChatBot!");
+});
+
+app.get('/webhooks/' , (req ,res)=>{
+	if(req.query['hub.verify_token'] === ''){
+		res.send(req.query['hub.challange']);
+	}
+	res.send("Wrong Token!");
+});
+
+app.listen(app.get('port') , ()=>{
+	console.log("Listening to Port " + app.get('port'));
+});
