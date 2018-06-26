b0d72d38f34e78df06641cc5e33e1c61c19305d0
MyDBot <40378113+MyDBot@users.noreply.github.com>
2018-06-26 22:04:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,66 @@
-'use strict';
+'use strict'
 
-const express   = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
 
+const app = express()
 
-const app = express();
+app.set('port', (process.env.PORT || 5000))
 
-app.set('port' , (process.env.PORT || 5000));
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// ROUTES
 
-let token = "EAADT0Fs73bIBACB4ZAMSTaGJY1k6chxIEvU9stQYP480ujejW0LYCEvITnU3jY7ZA9f2AcNY8tzYY8PH1WI4SoZBR7ujeiXQQg6pdnjxVGpZAcckNGRbkxSBY2SSdcP2K8heQelwuqEJZBOh6dKVmQIPkSuctnWSgz09yEFBflgZDZD";
+app.get('/', function(req, res) {
+	res.send("Hi I am a chatbot")
+})
 
-app.get('/' , (req ,res)=>{
-	res.send("Hi Im a ChatBot!");
-});
+let token = "EAADT0Fs73bIBACB4ZAMSTaGJY1k6chxIEvU9stQYP480ujejW0LYCEvITnU3jY7ZA9f2AcNY8tzYY8PH1WI4SoZBR7ujeiXQQg6pdnjxVGpZAcckNGRbkxSBY2SSdcP2K8heQelwuqEJZBOh6dKVmQIPkSuctnWSgz09yEFBflgZDZD"
 
-app.get('/webhooks/' , (req ,res)=>{
-	if(req.query['hub.verify_token'] === 'MyDABD'){
-		res.send(req.query['hub.challange']);
+// Facebook 
+
+app.get('/webhook/', function(req, res) {
+	if (req.query['hub.verify_token'] === "MyDABD") {
+		res.send(req.query['hub.challenge'])
 	}
-	else{
-		res.send("Wrong Token!");
-	}
-});
+	res.send("Wrong token")
+})
 
-app.post('/webhooks/' ,(req,res)=>{
-	let messaging_events = req.body.entrty[0].messaging;
-	for(let i = 0;i<messaging_events.length;i++){
-		let event = messaging_events[i];
-		let sender = event.sender.id;
-		if(event.message && event.message.text){
-			let text = event.message.text;
-			sendText(sender , 'Text Echo: '+ text.substring(0,100));
-
+app.post('/webhook/', function(req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
 		}
 	}
-	message.sendStatus(200);
-});
+	res.sendStatus(200)
+})
 
-
-function sendText(sender , text){
-	let messageData = {text: text};
+function sendText(sender, text) {
+	let messageData = {text: text}
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs: {access_token: token},
+		qs : {access_token: token},
 		method: "POST",
 		json: {
 			recipient: {id: sender},
-			message: messageData
-
-		},function(err , responce , body){
-			if(err){
-				console.log(err);
-			}
-			else if(responce.body.error){
-				console.log(responce.body.error);
-			}
+			message : messageData,
 		}
-	});
-}			
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
 
-app.listen(app.get('port') , ()=>{
-	console.log("Listening to Port " + app.get('port'));
-});
+app.listen(app.get('port'), function() {
+	console.log("running: port")
+})
